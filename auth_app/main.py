4fc7c6bf63849015de49470885b1c5077ce50bad
Matthew Chapman <mttchpmn07@gmail.com>
2024-02-13 23:48:32 -0600

    move keygen and crud functions into their own files

--- auth_app/main.py
+++ auth_app/main.py
@@ -1,11 +1,12 @@
-import secrets
 import validators
 from fastapi import Depends, FastAPI, HTTPException, Request
 from fastapi.responses import RedirectResponse
 from sqlalchemy.orm import Session
+from starlette.datastructures import URL
 
-from . import models, schemas
+from . import models, schemas, crud
 from .database import SessionLocal, engine
+from .config import get_settings
 
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
@@ -16,6 +17,15 @@ def get_db():
         yield db
     finally:
         db.close()
+
+def get_admin_info(db_url: models.URL) -> schemas.URLInfo:
+    base_url = URL(get_settings().base_url)
+    admin_endpoint = app.url_path_for(
+        "administration info", secret_key=db_url.secret_key
+    )
+    db_url.url = str(base_url.replace(path=db_url.key))
+    db_url.admin_url = str(base_url.replace(path=admin_endpoint))
+    return db_url
 
 def raise_bad_request(message):
     raise HTTPException(status_code=400, detail=message)
@@ -33,19 +43,8 @@ def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
     if not validators.url(url.target_url):
         raise_bad_request(message="Your provided URL is not valid")
     
-    chars = "ABCDEFGHIJKKLMN)PQRSTUVWXYZ"
-    key = "".join(secrets.choice(chars) for _ in range(5))
-    secret_key = "".join(secrets.choice(chars) for _ in range(8))
-    db_url = models.URL(
-        target_url=url.target_url, key=key, secret_key=secret_key
-    )
-    db.add(db_url)
-    db.commit()
-    db.refresh(db_url)
-    db_url.url = key
-    db_url.admin_url = secret_key
-
-    return db_url
+    db_url = crud.create_db_url(db=db, url=url)
+    return get_admin_info(db_url)
 
 @app.get("/{url_key}")
 def forward_to_target_url(
@@ -53,12 +52,20 @@ def forward_to_target_url(
     request: Request,
     db: Session = Depends(get_db)
 ):
-    db_url = (
-        db.query(models.URL)
-        .filter(models.URL.key == url_key, models.URL.is_active)
-        .first()
-    )
-    if db_url:
+    if db_url := crud.get_db_url_by_key(db=db, url_key=url_key):
         return RedirectResponse(db_url.target_url)
+    else:
+        raise_not_found(request)
+
+@app.get(
+    "/admin/{secret_key}",
+    name="administration info",
+    response_model=schemas.URLInfo,
+)
+def get_url_info(
+    secret_key: str, request: Request, db: Session = Depends(get_db)
+):
+    if db_url := crud.get_db_url_by_secret_key(db, secret_key=secret_key):
+        return get_admin_info(db_url)
     else:
         raise_not_found(request)
